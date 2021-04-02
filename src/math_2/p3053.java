f0f8385a8a227e6e5c81ff00af65147166457c22
jisoo <ske050588@gmail.com>
2021-04-02 18:59:23 +0900

    Math.round 와 String.format의 차이

--- src/math_2/p3053.java
+++ src/math_2/p3053.java
@@ -8,8 +8,11 @@ public class p3053 {
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		System.out.println(String.format("%.6f", Math.pow(x, 2)*Math.PI));//유클리드 기하학 r^2*PI
-		System.out.println(String.format("%.6f", Math.pow(x, 2)*2));//택시 기하학 2*(r^2)
+		System.out.println(String.format("%.6f", Math.pow(x, 2)*2));//택시 기하학 2*(r^2) : 2.000000
 sc.close();
+		//Math.round로 해보기
+	System.out.println(Math.round(Math.pow(x, 2)*Math.PI*1000000)/1000000.0);// 3.141593
+	System.out.println(Math.round(Math.pow(x, 2)*2*1000000)/1000000.0);// 2.0
 	}
 
 }
