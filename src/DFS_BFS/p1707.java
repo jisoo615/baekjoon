5b9b19384574ff460b6ecb8567f922e202926b70
jisoo <ske050588@gmail.com>
2021-08-24 13:09:24 +0900

    간선 저장하는 방법 인접리스트

--- src/DFS_BFS/p1707.java
+++ src/DFS_BFS/p1707.java
@@ -3,11 +3,12 @@ package DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class p1707 {
-	static int[] mat;
-	static boolean visited[];
+	static ArrayList<ArrayList<Integer>> graph;
+	static int visited[];
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,13 +17,24 @@ public class p1707 {
 		T = Integer.parseInt(br.readLine());
 		while(T>0) {
 			T--;
+			graph = new ArrayList<>();
+			
 			st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
 			
-			for(int i=0; i<E; i++) {
-				
+			for(int i=0; i<V; i++) {
+				graph.add(new ArrayList<Integer>() );
 			}
+			for(int i=0; i<E; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				int p1 = Integer.parseInt(st.nextToken());
+				int p2 = Integer.parseInt(st.nextToken());
+				graph.get(p1).add(p2);
+				graph.get(p2).add((p1));
+			}
+			
+			
 			
 		}
 		
