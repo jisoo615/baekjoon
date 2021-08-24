016d469cec7552ef8de02674ade6b0a365c92b91
jisoo <ske050588@gmail.com>
2021-08-24 22:28:57 +0900

    최단거리 다익스트라 그래프 우선순위큐

--- src/shortestPath/p1753.java
+++ src/shortestPath/p1753.java
@@ -3,11 +3,31 @@ package shortestPath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class p1753 {
-	static ArrayList<Node> list[];
+	static class Node implements Comparable<Node>{//static 붙여줘야 내부클래스로 바로 사용../왜냐 오류났었음
+		int v;
+		int w;
+		
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+			
+		}
+
+		@Override
+		public int compareTo(Node o) {//오름차순
+			return w - o.w;
+		}
+	}
+	
+	static LinkedList<Node> list[];
+	static int distance[];
+	static boolean visited[];
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,29 +35,50 @@ public class p1753 {
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 		int start = Integer.parseInt(br.readLine());
-	
+		distance = new int[V+1];
+		Arrays.fill(distance, -1);//-1로 초기화
+		visited = new boolean[V+1];
+		list = new LinkedList[V+1];
+		
+		for(int i=1; i<=V; i++) {
+			list[i] = new LinkedList<Node>();//
+		}
 		for(int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			
+			list[u].add(new Node(v, w));
 		}
-
+		
+		dijkstra(start);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=V; i++) {
+			sb.append(distance[i]==-1 ? "INF" : distance[i]).append("\n");
+		}
+		System.out.print(sb);
 	}
 	
-	class Node{
-		int u;
-		int v;
-		int w;
+	static public void dijkstra(int n) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(n, 0));
+		distance[n] = 0;
 		
-		public Node(int u, int v, int w) {
-			this.u = u;
-			this.v = v;
-			this.w = w;
-			
-		}
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			if(visited[ now.v ]==false) {
+				visited[ now.v ]=true;
+				for(Node next : list[now.v]) {
+					
+					if(distance[next.v]==-1 || distance[next.v] > distance[now.v] + next.w ) {
+						distance[next.v] = distance[now.v] + next.w;
+						pq.offer(new Node(next.v, distance[next.v]));//이젠 next.w가 아니라 distance[next.w]의 값을 가중치로 저장해야됨.
+					}
+				}//for end
+			}
+		}//while end
 	}
 
 }
