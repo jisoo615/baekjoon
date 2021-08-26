0ce3eed78c109d83ee262aeb978b6e6f71aadbb6
jisoo <ske050588@gmail.com>
2021-08-26 21:38:31 +0900

    최단경로 벨만포드 알고리즘

---
+++ src/shortestPath/p11657.java
@@ -0,0 +1,49 @@
+package shortestPath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class p11657 {
+	static int V, E;
+	static LinkedList<Node> list[];
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		for(int i=1; i<=V; i++) {
+			list[i] = new LinkedList<>();
+		}
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			list[u].add(new Node(v, w));
+		}
+		
+		//bellman-ford
+
+	}
+	
+	//bellman-ford
+	
+	static class Node implements Comparable<Node>{
+		int v;
+		int w;
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return w - o.w;
+		}
+	}
+
+}
