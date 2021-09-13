93e1ef7a5f020f562e521cb912da230b3e79ad54
jisoo <ske050588@gmail.com>
2021-09-13 22:09:48 +0900

    동적 계획법 기초 9095

---
+++ src/dynamic_1/p9095.java
@@ -0,0 +1,28 @@
+package dynamic_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램
+public class p9095 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		while(T>0) {
+			int n = Integer.parseInt(br.readLine());
+			T--;
+
+
+		}
+
+
+	}
+
+	static public int dp() {
+		return dp[]
+	}
+
+}
