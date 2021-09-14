1697441dd7527e5d4716adcdea13b8527b8477db
jisoo <ske050588@gmail.com>
2021-09-14 11:16:58 +0900

    동적프로그래밍 dp 9095 1, 2, 3 더하기

--- src/dynamic_1/p9095.java
+++ src/dynamic_1/p9095.java
@@ -5,24 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 //정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램
+//dp[n] = dp[n-1] + dp[n-2] + dp[n-3]
 public class p9095 {
+	static int[] dp;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-
-		while(T>0) {
+		StringBuilder sb = new StringBuilder();
+		while(T-- >0) {
 			int n = Integer.parseInt(br.readLine());
-			T--;
+			dp = new int[n+1];
 
-
+			sb.append(dp(n)).append("\n");
 		}
 
-
+		System.out.print(sb);
 	}
 
-	static public int dp() {
-		return dp[]
+	static public int dp(int n) {
+
+		if(n==1) return dp[1] = 1;
+		if(n==2) return dp[2] = 2;
+		if(n==3) return dp[3] = 4;
+
+		//if(n>=3)
+		dp[n] = dp(n-1) + dp(n-2) + dp(n-3);
+
+		return dp[n];
 	}
 
 }
